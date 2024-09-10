c2e7260d8e8ecd4d3262348d921fa69e60a292dc
theo geneste <theo.boxe@hotmail.fr>
2024-09-10 10:31:39 +0200

    Correction du JS

--- index.js
+++ index.js
@@ -7,14 +7,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskToDo = document.getElementById('task-todo');
     const taskDone = document.getElementById('task-done');
     const errorMessage = document.getElementById('error-message');
+    const countTotal = document.getElementById('total');
+    const countAFaire = document.getElementById('aFaire');
+    const countFaites = document.getElementById('faites');
 
     // Écoute le click du bouton et ajouter les li
     buttonTask.addEventListener('click', () => {
+        addTask();
+    });
+
+    input.addEventListener('keydown', (e) => {
+        if (e.key == "Enter") {
+            addTask();
+        }
+    })
+
+    function addTask(){
         if(input.value != null && input.value.length > 0){
             errorMessage.style.display = "none";
 
             //Création du LI
             const li = document.createElement('li');
+            li.classList.add("unchecked");
             
             // Création du checkbox
             let checkbox = document.createElement('input');
@@ -23,10 +37,16 @@ document.addEventListener('DOMContentLoaded', () => {
             // au changement de mon checkbox
             checkbox.addEventListener('change', () => {
                 if(checkbox.checked){
+                    li.classList.add("checked");
+                    li.classList.remove("unchecked");
                     taskDone.appendChild(li);
                 }else{
+                    li.classList.remove("checked");
+                    li.classList.add("unchecked");
                     taskToDo.appendChild(li);
                 }
+                countAFaire.innerText = taskToDo.childElementCount;
+                countFaites.innerText = taskDone.childElementCount;
             });
             // Ajout du checkbox dans mon li
             li.appendChild(checkbox);
@@ -42,7 +62,30 @@ document.addEventListener('DOMContentLoaded', () => {
             updateButton.innerHTML = "<i class='fa-solid fa-pen-to-square'></i>";
             // Function onClick sur le bouton modifier
             updateButton.addEventListener('click', () => {
-                console.log("je veux modifier");
+                // Cacher mes éléments déjà créé
+                checkbox.style.display = "none";
+                paragraphe.style.display = "none";
+                updateButton.style.display = "none";
+                deleteButton.style.display = "none";
+                // Création de mon input
+                const inputUpdate = document.createElement("input");
+                inputUpdate.setAttribute('type', "text");
+                inputUpdate.value = paragraphe.innerText;
+                inputUpdate.addEventListener('keydown', (e) => {
+                    if (e.key == "Enter") {
+                        updateTask(paragraphe,buttonSubmit, inputUpdate,checkbox,updateButton, deleteButton ); 
+                    }
+                })
+                // Création de mon bouton pour envoyer la nouvelle valeur
+                const buttonSubmit = document.createElement('button');
+                buttonSubmit.innerText = "Modifier";
+                buttonSubmit.addEventListener('click' , () => {
+                    updateTask(paragraphe,buttonSubmit, inputUpdate,checkbox,updateButton, deleteButton );
+                })
+
+                li.appendChild(inputUpdate);
+                li.appendChild(buttonSubmit);
+
             });
             // Ajout du bouton dans mon li
             li.appendChild(updateButton);
@@ -52,7 +95,10 @@ document.addEventListener('DOMContentLoaded', () => {
             deleteButton.innerHTML = "<i class='fa-solid fa-trash'></i>";
             // Function onClick sur le bouton supprimer
             deleteButton.addEventListener('click', () => {
-                console.log('Je veux supprimer');
+                li.remove();
+                countAFaire.innerText = taskToDo.childElementCount;
+                countFaites.innerText = taskDone.childElementCount;
+                countTotal.innerText = taskToDo.childElementCount + taskDone.childElementCount;
             });
             // Ajout du bouton dans mon li
             li.appendChild(deleteButton);
@@ -63,9 +109,27 @@ document.addEventListener('DOMContentLoaded', () => {
             // remettre à zero l'input
             input.value = "";
 
+            // Mettre à jour les compteurs à l'ajout d'une tache
+            countAFaire.innerText = taskToDo.childElementCount;
+            countTotal.innerText =  taskToDo.childElementCount + taskDone.childElementCount;
+            
+
         }else{
             errorMessage.style.display = "flex";
         }
-    })
+    }
 
+    function updateTask(paragraphe,buttonSubmit, inputUpdate,checkbox,updateButton, deleteButton ){
+        paragraphe.innerText = inputUpdate.value;
+
+        // Supprimer les éléments de modification input button
+        buttonSubmit.remove();
+        inputUpdate.remove();
+
+        // Faire réaparaitre les éléments
+        checkbox.style.display = "flex";
+        paragraphe.style.display = "flex";
+        updateButton.style.display = "flex";
+        deleteButton.style.display = "flex";
+    }
 })
